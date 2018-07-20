28bab54a6b041e78128313bd3c43fbc453f80747
ShaneDel <shanedel@gmail.com>
2018-07-19 21:39:30 -0500

    final commit

--- resources/js/trackster.js
+++ resources/js/trackster.js
@@ -1,13 +1,36 @@
+var Trackster = {};
+const API_KEY = 'ebe3a62d673ad7785a8f7ce995969ffa';
+
 $(document).ready(function () {
 
-  var Trackster = {};
-  const API_KEY = 'ebe3a62d673ad7785a8f7ce995969ffa';
+
   /*
     Given an array of track data, create the HTML for a Bootstrap row for each.
     Append each "row" to the container in the body to display all tracks. 
   */
   Trackster.renderTracks = function (tracks) {
+    var $trackList = $('#track-list');
 
+    $trackList.empty();
+
+    for (var trackIndex = 0; trackIndex < tracks.length; trackIndex++) {
+      var track = tracks[trackIndex];
+      var mediumAlbumArt = track.image[1]["#text"];
+      var htmlTrackRow =
+        '<div class="row track">' +
+        '  <div class="col-xs-1 col-xs-offset-1 play-button">' +
+        '    <a href="' + track.url + '" target="_blank">' +
+        '      <i class="fa fa-play-circle-o fa-2x"></i>' +
+        '    </a>' +
+        '  </div>' +
+        '  <div class="col-xs-4">' + track.name + '</div>' +
+        '  <div class="col-xs-2">' + track.artist + '</div>' +
+        '  <div class="col-xs-2"><img src="' + mediumAlbumArt + '"/></div>' +
+        '  <div class="col-xs-2">' + track.listeners + '</div>' +
+        '</div>';
+
+      $trackList.append(htmlTrackRow);
+    }
   };
 
   /*
@@ -16,9 +39,9 @@ $(document).ready(function () {
   */
   Trackster.searchTracksByTitle = function (title) {
     $.ajax({
-      url: 'https://ws.audioscrobbler.com/2.0/?method=track.search&track=' + title + 'tiny&api_key=' + API_KEY + '&format=json',
-      success: function(respons) {
-        Trackster.renderTracks(Response.results.trackmatches.track);
+      url: 'https://ws.audioscrobbler.com/2.0/?method=track.search&track=' + title + '&api_key=' + API_KEY + '&format=json',
+      success: function (response) {
+        Trackster.renderTracks(response.results.trackmatches.track);
       }
     })
   };
